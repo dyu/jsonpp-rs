26c10e22fef3dfd7d2ab5e074a85773f4cd32ad0
dyu <david.yu.ftw@gmail.com>
2023-05-04 17:14:37 +0800

    use mimalloc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use mimalloc::MiMalloc;
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 use std::ffi::{OsStr, OsString};
 use std::fs::{self, File};
 use std::io::{self, BufReader, BufWriter, Read};
